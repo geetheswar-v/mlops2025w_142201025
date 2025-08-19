836982e7cf5d25493a70ea6b5ee6e8cb88f393c2
geetheswar-v <geetheswar.edu@gmail.com>
2025-08-19 12:03:20 +0530

    Week3: Final Step modified Revision with Factorial functionality and shown difference

--- class/add_fac_rev.sh
+++ class/add_fac_rev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Adding Two Number
-echo "Given Two number A and B, Results SUM(A+B)"
+# Adding Two Number and Factorial
+echo "Given Two number A and B, Results SUM(A+B), Fac(A) and Fac(B)"
 
 # Inputs
 read -p "a: " a
@@ -12,5 +12,25 @@ echo "Given, A = ${a} and B = ${b}"
 # Sum of two numbers
 sum=$((a + b))
 
+# Factorial Function
+factorial() {
+  local n=$1
+  if (( n < 0 )); then
+    echo "negative number $n"
+    return
+  fi
+  if (( n == 0 || n == 1 )); then
+    echo 1
+    return
+  fi
+  local fact=1
+  for (( i=2; i<=n; i++ )); do
+    fact=$((fact * i))
+  done
+  echo $fact
+}
+
 # Result Echo
 echo "${a} + ${b} = ${sum}"
+echo "Factorial of A (${a}) = $(factorial "$a")"
+echo "Factorial of B (${b}) = $(factorial "$b")"
