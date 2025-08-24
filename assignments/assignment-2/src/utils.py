1cfdf239a215fad8bac6b40bb5f198e21e9bc881
geetheswar-v <geetheswar.edu@gmail.com>
2025-08-24 11:54:43 +0530

    [Q1] Implemented new Utilities

--- assignments/assignment-2/src/utils.py
+++ assignments/assignment-2/src/utils.py
@@ -4,9 +4,31 @@ def num_input(n: int, prompt: str) -> list[float]:
     numbers = []
     for i in range(n):
         while True:
-            try:
-                numbers.append(float(input(f'{prompt} ({i + 1}): ')))
+            s = input(f"{prompt} ({i + 1}): ")
+            if is_number(s, float):
+                numbers.append(float(s))
                 break
-            except ValueError:
-                print("Invalid input. Please enter a number.")
+            print("Invalid input. Please enter a number.")
     return numbers
+
+# Take a single integer input with minimum threshold
+def single_integer_input(prompt: str, min_value: int = 1) -> int:
+    while True:
+        s = input(f"{prompt}: ")
+        if is_number(s, int):
+            value = int(s)
+            if value >= min_value:
+                return value
+            print(f"Please enter a value greater than or equal to {min_value}.")
+        print("Invalid input. Please enter a valid integer.")
+
+# Check if a string is a valid number (int, float)
+def is_number(s: str, type: int | float) -> bool:
+    try:
+        if type == int:
+            int(s)
+        else:
+            float(s)
+        return True
+    except ValueError:
+        return False
