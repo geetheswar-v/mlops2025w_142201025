f9c772e6ddc08cb047e3b642bf85babe2e0ad1cc
geetheswar-v <geetheswar.edu@gmail.com>
2025-08-24 11:48:47 +0530

    [Q1] Implement Addition of Two Numbers Functionality in Calculator

--- assignments/assignment-2/src/calculator.py
+++ assignments/assignment-2/src/calculator.py
@@ -1,5 +1,13 @@
-def add(a, b):
+from utils import num_input
+
+def add(a: float, b: float) -> float:
     return a + b
 
-def main():
-    print("Welcome to the calculator!")
+def main() -> None:
+    print("Welcome to the Addition calculator!")
+    numbers = num_input(2, "Enter a number")
+    result = add(*numbers)
+    print(f"The Addition is: {result}")
+
+if __name__ == "__main__":
+    main()
