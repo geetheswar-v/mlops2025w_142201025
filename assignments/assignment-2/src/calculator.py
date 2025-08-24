40311be71872bcd1b11654f12ca6524ce0998ede
geetheswar-v <geetheswar.edu@gmail.com>
2025-08-24 11:57:00 +0530

    [Q1] N numbers Addition feature Added
    Completed the functionality of addition Calculator

--- assignments/assignment-2/src/calculator.py
+++ assignments/assignment-2/src/calculator.py
@@ -1,12 +1,13 @@
-from utils import num_input
+from utils import num_input, single_integer_input
 
 def add(a: float, b: float) -> float:
     return a + b
 
 def main() -> None:
     print("Welcome to the Addition calculator!")
-    numbers = num_input(2, "Enter a number")
-    result = add(*numbers)
+    n = single_integer_input("How many numbers do you want to add?", 2)
+    numbers = num_input(n, "Enter a number")
+    result = sum(numbers)
     print(f"The Addition is: {result}")
 
 if __name__ == "__main__":
