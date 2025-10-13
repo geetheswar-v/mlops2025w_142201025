95aaec4995e988119b91be52c499eb24f0331d4c
geetheswar-v <geetheswar.edu@gmail.com>
2025-10-13 19:20:20 +0530

    [Assignment 5] Updated Q1 | Removed Logs

--- assignments/assignment-5/src/assignment_5/conll2003_stats.py
+++ assignments/assignment-5/src/assignment_5/conll2003_stats.py
@@ -4,18 +4,15 @@ import wandb
 
 from assignment_5.datasets import get_conll2003_dataset
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("Question1")
-
 def summary(dataset, run=None):
-    logger.info("Checking dataset splits and their sizes")
+    print("Question1: Checking dataset splits and their sizes")
     for split, data in dataset.items():
         num_samples = len(data)
-        logger.info(f"Number of samples (sentences) in {split} split: {num_samples}")
+        print(f"Question1: Number of samples (sentences) in {split} split: {num_samples}")
         if run:
             run.summary[f"{split}_num_samples_sentences"] = num_samples
 
-    logger.info("Calculating Entity distribution of each split")
+    print("Question1: Calculating Entity distribution of each split")
     entity_to_bid = {'O': 0, 'PER': 1, 'ORG': 3, 'LOC': 5, 'MISC': 7}
     bid_to_entity = {v: k for k, v in entity_to_bid.items() if k != 'O'}
 
@@ -29,19 +26,19 @@ def summary(dataset, run=None):
                     entity_name = bid_to_entity[tag_id]
                     main_entity_counts[entity_name] += 1
         
-        logger.info(f"Entity counts for {split} split: {dict(main_entity_counts)}")
+        print(f"Question1: Entity counts for {split} split: {dict(main_entity_counts)}")
         
         if run:
             for entity, count in main_entity_counts.items():
                 run.summary[f"{split}_{entity}_entity_count"] = count
             run.summary[f"{split}_total_entities"] = sum(main_entity_counts.values())
 
-    logger.info("Summary computation and logging completed successfully.")
+    print("Question1: Summary computation and logging completed successfully.")
     
 
 def main():
     project_name = "Q1-weak-supervision-ner"
-    run = wandb.init(project=project_name, job_type="dataset-analysis", name="test_2")
+    run = wandb.init(project=project_name, job_type="dataset-analysis", name="Test 1")
     
     dataset = get_conll2003_dataset()
     summary(dataset, run)
--- assignments/assignment-5/src/assignment_5/datasets.py
+++ assignments/assignment-5/src/assignment_5/datasets.py
@@ -1,13 +1,10 @@
 import logging
 from datasets import load_dataset
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 def get_conll2003_dataset():
-    logger.info("Loading CONLL2003 dataset...")
+    print("Loading CONLL2003 dataset...")
     dataset = load_dataset("eriktks/conll2003", revision="convert/parquet")
-    logger.info("CONLL2003 dataset loaded successfully.")
+    print("CONLL2003 dataset loaded successfully.")
     dataset_splits = {}
     for split in dataset.keys():
         dataset_splits[split] = dataset[split]
